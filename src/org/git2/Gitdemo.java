26d6b450f1a98a62c2f74e378097417f09344597
Shruthi006 <Shruthicse45@gmail.com>
2019-06-13 22:51:09 +0530

    modified first class

--- src/org/git2/Gitdemo.java
+++ src/org/git2/Gitdemo.java
@@ -3,10 +3,12 @@ package org.git2;
 public class Gitdemo {
 
 	public static void main(String[] args) {
-System.out.println("first class");
-	
-}
+		
+		System.out.println("first class");
+		System.out.println("first class");
+		System.out.println("first class");
+		System.out.println("first class");
 
 	}
 
-
+}
